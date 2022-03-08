aedeb2932062dd78f0a10f7f97e1795699078055
jacobyi11 <jakeyi00@gmail.com>
2022-03-08 11:18:59 -0800

    in progress

--- Arcade 4-8/main.js
+++ Arcade 4-8/main.js
@@ -8,5 +8,4 @@ function solution(inputArray) {
     }
   }
   return largestProduct;
-
 }
--- Practice Problems/main.js
+++ Practice Problems/main.js
@@ -2,7 +2,8 @@
 
 function swap(tuple) {
   tuple.push(tuple[0]);
-  tuple.splice(1,1);
+  tuple.splice(0,1);
+  return tuple
 }
 
 
@@ -22,57 +23,48 @@ function factorial(number) {
 function fuzeBuzz(limit) {
   for (var i = 0; i < limit; i++) {
     if (i % 15 === 0) {
-      console.log('11')
+      console.log('1')
     } else if ( i % 3 === 0 ) {
-      console.log('1')
+      console.log('2')
     } else if(i % 5 === 0) {
-      console.log('1')
+      console.log('3')
     }
   }
 }
 
 // Level 4
 
-// function charCounts(string) {
-//   var obj = {};
-//   for (var char in string) {
-//     if (obj[char]) {
-//       obj[char]++
-//     } else {
-//       obj[char] = 1
-//     }
-//   }
-//   return obj
-// }
-
-// const countChar = (str) {
-//   const counts = {};
-//   for (const s of str) {
-//     if (counts[s]) {
-//       counts[s]++
-//     } else {
-//       counts[s] = 1
-//     }
-//   }
-//   return counts;
-// }
-
-// program to check the number of occurrence of a character
-
 function countAmountOfEachCharacter(inputString) {
   const returnObject = {};
-
-  // loop over input string
   for (let i = 0; i < inputString.length; i++) {
-    // check if character has been seen before
     if (returnObject.hasOwnProperty(inputString[i])) {
-      // increase its count by 1
       returnObject[inputString[i]] += 1;
     } else {
-      // add it to the object with a count of 1
       returnObject[inputString[i]] = 1;
     }
   }
-
   return returnObject;
+}
+
+// Level 5
+
+function unique(str) {
+  var string = '';
+  for (var i = 0; i < str.length; i++) {
+    if (string.includes(str[i]) === false) {
+      string += str[i]
+    }
+  }
+  return string
+}
+
+// Level 6
+
+function sumAll(numbers) {
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    sum += number;
+  }
+  return sum;
 }

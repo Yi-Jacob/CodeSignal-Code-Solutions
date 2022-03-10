cfbe29c8b9957b4c524c039f9897593fb2688219
jacobyi11 <jakeyi00@gmail.com>
2022-03-10 15:07:04 -0800

    level 6

--- Arcade 4-8/main.js
+++ Arcade 4-8/main.js
@@ -14,3 +14,17 @@ function solution(inputArray) {
 function solution(n) {
   return n * n + (n - 1) * (n - 1)
 }
+
+// Level 6
+function solution(statues) {
+  let statuesNeeded = 0;
+  statues.sort((a, b) => {
+    return a - b;
+  })
+  for (let i = 0; i < statues.length; i++) {
+    if (statues[i + 1] - statues[i] > 1) {
+      statuesNeeded += statues[i + 1] - statues[i] - 1;
+    }
+  }
+  return statuesNeeded;
+}

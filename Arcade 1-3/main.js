b3a47ac24e6aa1888cf26f9c2a472afa125f12e0
jacobyi11 <jakeyi00@gmail.com>
2022-03-10 15:16:11 -0800

    added psuedo code

--- Arcade 1-3/main.js
+++ Arcade 1-3/main.js
@@ -1,9 +1,17 @@
 // Level 1
+/*
+add the 2 parameters
+*/
 function solution(param1, param2) {
   return param1 + param2;
 }
 
 // Level 2
+/*
+get the century of the year by dividing by 100
+if its the century return the century number
+if its not round down then add a year
+*/
 function solution(year) {
   var century = year / 100
   if (year % 100 === 0) {
@@ -14,6 +22,15 @@ function solution(year) {
 }
 
 // Level 3
+/*
+get a new string to get rid of spaces by replacing spaces with no spaces
+get the lenght of the string and assign it to a variable
+loop through the first half of the string
+get the value of each index and assign it to a variable
+get the values of the back half
+if they are not equal it is not a palindrome
+*/
+
 function solution(string) {
   var newString = string.replace(' ', '');
   var length = newString.length - 1;

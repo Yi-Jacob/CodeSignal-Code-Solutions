1003b49e9b1136c873a68dce4b870fa50548db15
jacobyi11 <jakeyi00@gmail.com>
2022-03-10 15:53:22 -0800

    level 8

--- Arcade 4-8/main.js
+++ Arcade 4-8/main.js
@@ -78,3 +78,35 @@ function solution(sequence) {
   }
   return true;
 }
+
+
+// Level 8
+/*
+set a variable to hold the sum
+loop through all the arrays
+then loop thorugh the indiviual arrays in the main array
+if the value of the array is zero then make all the values in that column of values zero
+then loop through the arrays and array in arrays
+add the numbers to the placeholder
+return the toal
+*/
+
+function solution(matrix) {
+  let total = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        for (let k = i + 1; k < matrix.length; k++) {
+          matrix[k][j] = 0;
+        }
+      }
+    }
+  }
+  for (let x = 0; x < matrix.length; x++) {
+    for (let y = 0; y < matrix[x].length; y++) {
+      total += matrix[x][y]
+    }
+  }
+  return total;
+}
